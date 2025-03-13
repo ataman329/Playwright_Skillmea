c3a7605d194ff7aa9bd12e2e146444b0a1c5db23
ataman329 <adrian.kozak89@gmail.com>
2025-03-13 10:44:00 +0100

    Vymazal som posledny riadok

--- tests/1testing.test.spec.ts
+++ tests/1testing.test.spec.ts
@@ -5,6 +5,5 @@ test('Test na git', async ({ page }) => {
     await page.locator('svg').first().click();
     await page.getByText('Radio Button').click();
     await page.getByText('Yes').click();
-    await page.getByText('Check Box').click();
-    await page.locator('#tree-node').getByRole('img').nth(3).click();    
+    await page.getByText('Check Box').click();  
 });
